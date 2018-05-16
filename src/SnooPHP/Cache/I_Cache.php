f00e7231ba9b00f5cae29e3dcea7c1ecd7aa9ff7
Sneppy <andrea.mecchia@mail.polimi.it>
2018-05-16 15:38:26 +0200

    Create project

---
+++ src/SnooPHP/Cache/I_Cache.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SnooPHP\Facebook;
+
+/**
+ * Interface to store and retrieve api requests
+ * 
+ * @author Sneppy
+ */
+interface I_Cache
+{
+	/**
+	 * Fetch a record from cache
+	 * 
+	 * @param string $key unique key of the record
+	 * 
+	 * @return object|null
+	 */
+	public static function fetch($key);
+
+	/**
+	 * Store a record in cache
+	 * 
+	 * @param string	$key		key of the record
+	 * @param string	$content	record content
+	 * 
+	 * @return object|null
+	 */
+	public static function store($key, $content);
+}
